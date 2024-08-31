ccd5d2084c0e70020803e48c3306a51aa6b2608d
Andrew Ghazi <6763470+andrewGhazi@users.noreply.github.com>
2024-08-31 17:13:10 -0400

    change message

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach = function(libname, pkgname) {
   ver = utils::packageVersion("fsummary")
 
-  cli::cli_alert_info("Use {.code mirai::daemons(2)} to start background daemons for parallelization.")
+  cli::cli_alert_info("Use {.code mirai::daemons(4)} to start background daemons for parallelization.")
 
 
 }
