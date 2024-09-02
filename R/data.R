da0116549fa7b2e451f1fb8d9d8f245620e6c3a7
Andrew Ghazi <6763470+andrewGhazi@users.noreply.github.com>
2024-09-02 13:48:43 -0400

    test case with zero variance variable

--- R/data.R
+++ R/data.R
@@ -18,3 +18,17 @@
 #' ddf, see the generating script in data-raw/
 #' @format ## `big_test_res` A tibble from posterior::summarise_draws(test_ddf)
 "big_test_res"
+
+#' zero variance draws data frame
+#'
+#' A test draws data frame where some parameters have 0 variance, which should produce some NAs.
+#' @format ## `na_ddf`
+#' A draws_df of three parameters, V1, V2, and V3, all numeric with 100 iterations and four chains.
+"na_ddf"
+
+#' zero variance summary result
+#'
+#' A test summary result where some parameters have 0 variance, which should produce some NAs.
+#' @format ## `na_res`
+#' A tibble from posterior::summarise_draws(test_ddf)
+"na_res"
